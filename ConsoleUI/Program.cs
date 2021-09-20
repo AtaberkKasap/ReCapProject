034a0fdc9cd4b65f95a3568d70409e1c0ac936a8
Ataberk Kasap <c.ataberkkasap@hotmail.com>
2021-09-21 00:20:47 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,24 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List <Car> cars = new List<Car> { 
+                new Car{CarId = 1, BrandId = 1, ColorId = 1, DailyPrice = 800, Description = "S90", ModelYear = "2020" },
+                new Car{CarId = 2, BrandId = 1, ColorId = 1, DailyPrice = 1000, Description = "XC90", ModelYear = "2020" },
+            };
+            CarManager carManager = new CarManager(new InMemoryCarDal(cars));
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,47 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal(List<Car> cars)
+        {
+            _cars = cars;
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            var carToDelete = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public Car GetById(int carId)
+        {
+            return _cars.SingleOrDefault(c => c.CarId == carId);
+        }
+
+        public void Update(Car car)
+        {
+            Car carToBeUpdated = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            carToBeUpdated = car;
+        }
+    }
+}
